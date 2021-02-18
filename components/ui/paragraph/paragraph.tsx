9a33ceed8a76177cbe65ad64ec1d4335683bf62b
Eden <eden@bit.dev>
2021-02-18 03:37:30 +0200

    ...

---
+++ components/ui/paragraph/paragraph.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Paragraph = ({ children }) => {
+  return <p>{children}</p>;
+};
---
+++ components/ui/tech-jokes-viewer/tech-jokes-viewer.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react';
+import styles from './tech-jokes-viewer.module.scss';
+import { Button } from '@demo-org/tech-jokes.ui.button';
+import { useJokes } from '@demo-org/tech-jokes.hooks.use-jokes';
+
+export type BadJokesProps = {
+  local: boolean;
+  className?: string;
+};
+
+/** Retrieves and displays bad jokes */
+export const TechJokesViewer = ({ local, className }: BadJokesProps) => {
+  const [
+    setIsLocal,
+    getJoke,
+    joke,
+    error,
+    disableGetJoke,
+    saveJoke,
+    removeJoke,
+  ] = useJokes(local);
+
+  const renderJoke = (text: string) => {
+    const textArr = text.split('\n');
+    return textArr.map((line, key) => <p key={key}>{line}</p>);
+  };
+
+  useEffect(() => {
+    setIsLocal(local);
+  }, [local]);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.contentWrapper}>{error || renderJoke(joke)}</div>
+      <div className={styles.buttonsWrapper}>
+        <Button disabled={disableGetJoke} onClick={getJoke}>
+          {disableGetJoke ? 'loading...' : 'another one, please'}
+        </Button>
+        <Button
+          variant="secondary"
+          disabled={disableGetJoke}
+          onClick={() =>
+            local ? removeJoke(joke, getJoke) : saveJoke(joke, getJoke)
+          }
+        >
+          {local ? 'remove joke' : 'save joke'}
+        </Button>
+      </div>
+    </div>
+  );
+};
