1a5b2eb1304e5d6ba9f79975e8db25bd4686bef6
Eden <eden@bit.dev>
2021-02-23 03:24:54 +0200

    updated .bitmap file after a successful export

--- components/widgets/tech-jokes-viewer/tech-jokes-viewer.composition.tsx
+++ components/widgets/tech-jokes-viewer/tech-jokes-viewer.composition.tsx
@@ -1,28 +1,25 @@
 import React, { useState } from 'react';
 import { TechJokesViewer } from './tech-jokes-viewer';
 
-// 1. UNCOMMENT THE FOLLOWING IMPORT
-// 2. REPLACE 'demo-org' WITH YOUR OWN ORGANIZATION/USERNAME
+import { AppBar } from '@demo-org/demo-scope.ui.elements.app-bar';
 
-// import { AppBar } from '@demo-org/demo-scope.ui.elements.app-bar';
+export const ToggleBetweenRemoteAndLocalJokes = () => {
+  const [isLocal, setIsLocal] = useState(false);
 
-// export const ToggleBetweenRemoteAndLocalJokes = () => {
-//   const [isLocal, setIsLocal] = useState(false);
+  const menuItems = [
+    { label: 'Explore New Jokes', action: () => setIsLocal(false) },
+    { label: 'Saved Jokes', action: () => setIsLocal(true) },
+  ];
 
-//   const menuItems = [
-//     { label: 'Explore New Jokes', action: () => setIsLocal(false) },
-//     { label: 'Saved Jokes', action: () => setIsLocal(true) },
-//   ];
-
-//   return (
-//     <div style={{ display: 'flex', justifyContent: 'center' }}>
-//       <div>
-//         <AppBar items={menuItems} style={{ marginBottom: '25px' }} />
-//         <TechJokesViewer local={isLocal} />
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <div>
+        <AppBar items={menuItems} style={{ marginBottom: '25px' }} />
+        <TechJokesViewer local={isLocal} />
+      </div>
+    </div>
+  );
+};
 
 export const RemoteJokes = () => {
   return (
